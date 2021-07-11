af4e3e4a6e9bb52756ce844c959532c979388c90
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-11 17:04:53 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+export default function App() {
+  console.log('Teste no console do navegador');
+
+  return (
+    <div>
+      <header>
+        <div className="bg-green-400 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl">
+            Projeto React Countries
+          </h1>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mx-auto p-4">
+          <h2>O conteúdo fica aqui.</h2>
+        </div>
+      </main>
+    </div>
+  );
+}
