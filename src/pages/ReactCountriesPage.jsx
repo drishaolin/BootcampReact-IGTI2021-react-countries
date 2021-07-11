23049982fd77a1b161bba1fc19f0dac14b9cec7c
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-11 17:46:37 -0300

    reuse of components Header, Main and TextInput

--- src/pages/ReactCountriesPage.jsx
+++ src/pages/ReactCountriesPage.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
+import Header from '../components/Header'
+import Main from '../components/Main'
 
 export default function ReactCountriesPage() {
     return (
         <div>
-             <header>
-        <div className="bg-green-400 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
+            <Header>
             Projeto React Countries
-          </h1>
-        </div>
-      </header>
+            </Header>
+       <Main>conteúdo</Main>
 
-      <main>
-        <div className="container mx-auto p-4">
-          <h2>O conteúdo fica aqui.</h2>
-        </div>
-      </main>
+     
         </div>
     )
 }
