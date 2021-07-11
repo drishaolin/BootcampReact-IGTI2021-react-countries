957ddaaf4876fd7e4969b092b7d4f217363dbf63
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-11 17:33:39 -0300

    create ReactCountriesPage

---
+++ src/pages/ReactCountriesPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function ReactCountriesPage() {
+    return (
+        <div>
+             <header>
+        <div className="bg-green-400 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl">
+            Projeto React Countries
+          </h1>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mx-auto p-4">
+          <h2>O conteúdo fica aqui.</h2>
+        </div>
+      </main>
+        </div>
+    )
+}
